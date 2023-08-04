bd5e7d35328fc3bb7bc9dd59e197c2620bf1b0b9
yujiangjun <18512106361>
2023-08-04 15:39:10 +0800

    增加注释

--- src/class02/Code01_ChooseWork.java
+++ src/class02/Code01_ChooseWork.java
@@ -30,12 +30,17 @@ public class Code01_ChooseWork {
 		map.put(job[0].hard, job[0].money);
 		// pre : 上一份进入map的工作
 		Job pre = job[0];
+		// 对于hard相同但是money不同的工作，取money最多的工作
+		// 同时 对于hard不同的工作，取hard越大，money越多的工作
+		// 不会取hard大但是money小的工作
+		// (1,3), (1,2) (2,1) (3,4) 中 (1,2)（2,1)这两个久不会取
 		for (int i = 1; i < job.length; i++) {
 			if (job[i].hard != pre.hard && job[i].money > pre.money) {
 				pre = job[i];
 				map.put(pre.hard, pre.money);
 			}
 		}
+		// 取小于alility离他最近的
 		int[] ans = new int[ability.length];
 		for (int i = 0; i < ability.length; i++) {
 			// ability[i] 当前人的能力 <= ability[i]  且离它最近的
